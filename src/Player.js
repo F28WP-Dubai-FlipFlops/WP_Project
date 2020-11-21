9bc390c4741db90366eec961309f7421b5067cc2
gemdias55 <gd55@hw.ac.uk>
2020-11-21 21:59:16 +0400

    Separated display code from js classes

--- src/Player.js
+++ src/Player.js
@@ -1,10 +1,10 @@
+const playerWidth = 80;
+const playerHeight = 80;
+
 // Class to represent player in javascript
-function Player(username, x, y) {
-  this.htmlElement = createPlayer(x, y);
-  this.nameTag = createNameTag(username, this.htmlElement);
-  this.parent = this.htmlElement.parentElement;
-  this.width = this.htmlElement.offsetWidth;
-  this.height = this.htmlElement.offsetHeight; 
+function Player(id, username, x, y) {
+  this.id = id;
+  this.username = username;
   
   this.position = {
     x: x, 
@@ -22,12 +22,12 @@ function Player(username, x, y) {
 
   // Gets x value of centre of player div
   this.getCentreX = function() {
-    return this.position.x + this.width / 2;
+    return this.position.x + playerWidth / 2;
   }
 
   // Gets y value of centre of player div
   this.getCentreY = function() {
-    return this.position.y + this.height / 2;
+    return this.position.y + playerHeight / 2;
   }
 
   // Ensures player stays within edges of the screen
@@ -36,8 +36,8 @@ function Player(username, x, y) {
     if (this.position.x < 0) {
       this.position.x = 0;
       this.velocity.x = 0;
-    } else if (this.position.x + this.width > this.parent.offsetWidth) {
-      this.position.x = this.parent.offsetWidth - this.width;
+    } else if (this.position.x + playerWidth > mapWidth) {
+      this.position.x = mapWidth - playerWidth;
       this.velocity.x = 0;
     }
 
@@ -45,8 +45,8 @@ function Player(username, x, y) {
     if (this.position.y < 0) {
       this.position.y = 0;
       this.velocity.y = 0;
-    } else if (this.position.y + this.height + 20 > this.parent.offsetHeight) {
-      this.position.y = this.parent.offsetHeight - this.height - 20;
+    } else if (this.position.y + playerHeight + 20 > mapHeight) {
+      this.position.y = mapHeight - playerHeight - 20;
       this.velocity.y = 0;
     }
   };
@@ -73,21 +73,6 @@ function Player(username, x, y) {
     
     this.aimAngle = angle_rad;
   };
-  
-  // Updates position of html divs holding the player and name tag
-  this.updatePos = function() {
-    // Move player on the game screen
-    this.htmlElement.style.left = this.position.x + "px";
-    this.htmlElement.style.top = this.position.y + "px";
-
-    // Rotate player
-    this.htmlElement.style.transform = "rotate(" + this.aimAngle + "rad)";
-
-    // Move name tag on the game screen
-    this.nameTag.style.left = (this.getCentreX() 
-                              - (this.nameTag.offsetWidth / 2)) + "px";
-    this.nameTag.style.top = (this.position.y + this.height) + "px";
-  };
 
   // Calculate new position of player
   this.move = function(dx, dy) {
@@ -105,69 +90,19 @@ function Player(username, x, y) {
     this.velocity.y *= 0.98;
 
     this.keepWithinBounds();
-    this.updatePos();
   };
 
-  // Create a new laser div
-  this.shoot = function() {
-    // Shoot a laser if cooldown is off
-    if (this.canShoot) {
-      let shot = document.createElement("div");
-      shot.setAttribute("class", "laser");
-      shot.style.transform = "rotate(" + this.aimAngle + "rad)";
-      gameScreen.appendChild(shot);
+  // Applies a shoot cooldown on the player
+  this.applyShootCooldown = function() {
+    this.canShoot = false;
 
-      laserShots.push(new Laser(this, shot));
-
-      // Reset cooldown
-      this.canShoot = false;
-      setTimeout(() => {
-        this.canShoot = true;
-      }, 1000);
-    }
+    setTimeout(() => {
+      this.canShoot = true;
+    }, 1500);
   };
 
   // Calculates player health and points when hit
   this.takeDamage = function() {
-    // Play an animation by adding a class to player div
-    this.htmlElement.setAttribute("class", "player damage");
-
-    // Remove the animation class after animation is over
-    setTimeout(() => {
-      this.htmlElement.setAttribute("class", "player");
-    }, 300);
+    // TODO
   };
 }
-
-
-// Create a new div to represent player object
-let createPlayer = function(x, y) {
-  let player = document.createElement("div");
-  player.setAttribute("class", "player");
-
-  // (Temp.) Set player image randomly
-  let imgColor = Math.floor(Math.random() * 8);
-  player.style.backgroundImage = "url(/images/spaceship" + imgColor + ".png)";
-  
-  // Set position of player div and add to html
-  player.style.left = x + "px";
-  player.style.top = y + "px";
-  gameScreen.appendChild(player);
-
-  return player;
-};
-
-// Create a new div to hold player's name tag
-let createNameTag = function(username, player) {
-  let nameTag = document.createElement("div");
-  nameTag.setAttribute("class", "username");
-  nameTag.innerHTML = username;
-
-  // Centre name tag and set its position 5px below player
-  nameTag.style.left = (player.offsetLeft + (player.offsetWidth / 2) 
-                        - (nameTag.offsetWidth / 2)) + "px";
-  nameTag.style.top = (player.offsetTop + player.offsetHeight) + "px";
-  gameScreen.appendChild(nameTag);
-
-  return nameTag;
-};
--- src/game.js
+++ src/game.js
@@ -1,61 +1,74 @@
 
 const cameraDiv = document.getElementById("camera");
 const gameScreen = document.getElementById("gameScreen");
+const mapWidth = gameScreen.offsetWidth;
+const mapHeight = gameScreen.offsetHeight;
 
-// Arrays to store all Player and Laser objects
-let players = [];
-let laserShots = [];
+// Objects to store all player, name tag and laser objects
+let players = {};
+let laserShots = {};
+let playerDivs = {};
+let nameTagDivs = {};
+let laserDivs = {};
 
 // (Temp.) Add two players
-players.push(new Player("Player1", 0, 0));
-players.push(new Player("Player2", 1200, 600));
+createPlayer(1, "Player1", 0, 0);
+createPlayer(2, "Player2", 500, 200);
 
 
-const camera = new Camera(cameraDiv, gameScreen, players[0]);
+const camera = new Camera(cameraDiv, gameScreen, players[1]);
 const controller = new Controller();
 
+// Variables used to calculate positions independant of frame rate
+let startTime = Date.now();
+let deltaTime;
 
 // Function called every time new frame is drawn
-let gameLoop = function() {
+function gameLoop() {
+  // Calculate time since last frame was drawn
+  deltaTime = ((Date.now() - startTime) / 1000) * 60;
   
   // Controls affect all players the same for now; used for testing
-  for (let i = 0; i < players.length; i++) {
+  for (let i in players) {
     let dx = 0;
     let dy = 0;
     
     // Calculate change in velocity based on key presses
-    if (controller.left) dx += -1;
-    if (controller.right) dx += 1;
+    if (controller.left) dx += -1 * deltaTime;
+    if (controller.right) dx += 1 * deltaTime;
     
-    if (controller.up) dy += -1;
-    if (controller.down) dy += 1;
+    if (controller.up) dy += -1 * deltaTime;
+    if (controller.down) dy += 1 * deltaTime;
 
     players[i].move(dx, dy);
 
-    if (controller.shoot) {
-      players[i].shoot();
+    // If player is able to shoot, make a laser and set a cooldown
+    if (controller.shoot && players[i].canShoot) {
+      createNewLaser(i);
+      players[i].applyShootCooldown();
     }
   }
 
-  for (let i = laserShots.length - 1; i >= 0; i--) {
-    laserShots[i].updatePos();
+  for (let i in laserShots) {
+    laserShots[i].move(deltaTime);
 
-    // If laser hit a player, handle player damage and remove the laser
-    for (let j = players.length - 1; j >= 0; j--) {
-      if (laserShots[i] !== undefined && laserShots[i].hitPlayer(players[j])) {
-        players[j].takeDamage();
-        laserShots[i].removeLaser();
-      }
+    if (laserShots[i].isOutOfBounds()) {
+      removeLaser(i);
     }
   }
   
   
-  camera.moveCamera();
-  
+  checkHits();               // Checks if lasers have hit a player
+  updateDisplay();           // Moves html divs
+  camera.moveCamera();       // Moves camera
+
+
+  startTime = Date.now();    // Stores time when this frame was drawn
   window.requestAnimationFrame(gameLoop);
 }
 
 
+// Event listeners
 window.addEventListener("keydown", (e) => controller.keyListener(e));
 window.addEventListener("keyup", (e) => controller.keyListener(e));
 
@@ -63,10 +76,144 @@ window.addEventListener("mousemove", (e) => {
   controller.mouseListener(e, camera.getCamX(), camera.getCamY());
   
   // Recalculates the aim angle of the player
-  players.forEach(player => {
-    player.setAimAngle(controller.mouseX, controller.mouseY);
-  });
+  for (let i in players) {
+    players[i].setAimAngle(controller.mouseX, controller.mouseY);
+  };
 });
 
 // Start game loop
 window.requestAnimationFrame(gameLoop);
+
+
+
+// Create a new player object
+function createPlayer(id, username, x, y) {
+  // Make a new player object (within js)
+  let player = new Player(id, username, x, y);
+  players[id] = player;
+
+
+  // Make a html div to represent player
+  let playerDiv = document.createElement("div");
+  playerDiv.setAttribute("class", "player");
+
+  let imgColor = Math.floor(Math.random() * 8) + 1;
+  playerDiv.style.backgroundImage = "url(/images/spaceship" + imgColor + ".png)";
+  
+  // Set position of player div and add to html
+  playerDiv.style.left = x + "px";
+  playerDiv.style.top = y + "px";
+  gameScreen.appendChild(playerDiv);
+
+
+  // Create a name tag for the player
+  let nameTag = document.createElement("div");
+  nameTag.setAttribute("class", "username");
+  nameTag.innerHTML = username;
+
+  // Centre name tag and set its position 5px below player
+  nameTag.style.left = (playerDiv.offsetLeft + (playerDiv.offsetWidth / 2) 
+                        - (nameTag.offsetWidth / 2)) + "px";
+  nameTag.style.top = (playerDiv.offsetTop + playerDiv.offsetHeight) + "px";
+  gameScreen.appendChild(nameTag);
+
+  
+  playerDivs[id] = playerDiv;
+  nameTagDivs[id] = nameTag;
+}
+
+// Create a new laser object
+function createNewLaser(playerId) {
+  // Generate a unique laser id
+  let laserId;
+  do {
+    laserId = Math.floor(Math.random() * 1000);
+  } while(laserId in laserShots);
+
+  // Calulate position and direction of laser and make the laser object
+  let x = players[playerId].getCentreX();
+  let y = players[playerId].getCentreY();
+  let angle = players[playerId].aimAngle;
+  let laser = new Laser(laserId, playerId, x, y, angle);
+
+  // Create an html div to represent laser
+  let laserDiv = document.createElement("div");
+  laserDiv.setAttribute("class", "laser");
+  laserDiv.style.transform = "rotate(" + angle + "rad)";
+  gameScreen.appendChild(laserDiv);
+  
+  
+  laserShots[laserId] = laser;
+  laserDivs[laserId] = laserDiv;
+}
+
+// Checks if any laser has hit a player
+function checkHits() {
+  for (let i in laserShots) {
+    for (let j in players) {
+      if (laserShots[i].playerId !== j) {
+        // Constants for centre x and y positions and radius of player and laser
+        const playerX = players[j].getCentreX();
+        const playerY = players[j].getCentreY();
+        const laserX = laserShots[i].getCentreX();
+        const laserY = laserShots[i].getCentreY();
+        const laserR = laserWidth / 2;
+        const playerR = playerWidth / 2;
+
+        // Calculate distance between player and laser
+        const distance = Math.sqrt(((playerX - laserX) ** 2) 
+                        + ((playerY - laserY) ** 2));
+        
+        // If the distance is less then the sum of their of radii, the laser 
+        // hit the player
+        if (distance < playerR + laserR) {
+          removeLaser(i);
+          players[j].takeDamage();
+          damageEffect(j);
+          break;
+        }
+      }
+    }
+  }
+}
+
+// Deletes a laser object and its html div
+function removeLaser(laserId) {
+  gameScreen.removeChild(laserDivs[laserId]);
+  delete laserShots[laserId];
+  delete laserDivs[laserId]
+}
+
+// Play an animation when a player takes damage
+function damageEffect(playerId) {
+  // Play the animation by adding a class to player div
+  playerDivs[playerId].setAttribute("class", "player damage");
+
+  // Remove the animation class after animation is over
+  setTimeout(() => {
+    playerDivs[playerId].setAttribute("class", "player");
+  }, 300);
+}
+
+
+// Updates positions of all html elements
+function updateDisplay() {
+  for (let i in players) {
+    // Move player on the game screen
+    playerDivs[i].style.left = players[i].position.x + "px";
+    playerDivs[i].style.top = players[i].position.y + "px";
+
+    // Rotate player
+    playerDivs[i].style.transform = "rotate(" + players[i].aimAngle + "rad)";
+
+    // Move name tag on the game screen
+    nameTagDivs[i].style.left = (players[i].getCentreX() 
+                              - (nameTagDivs[i].offsetWidth / 2)) + "px";
+    nameTagDivs[i].style.top = (players[i].position.y + playerHeight) + "px";
+  }
+
+  for (let i in laserShots) {
+    laserDivs[i].style.left = laserShots[i].position.x + "px";
+    laserDivs[i].style.top = laserShots[i].position.y + "px"; 
+  }
+}
