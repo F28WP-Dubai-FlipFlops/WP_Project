07fdc4d333d071278b0e365f38151521c055f070
gemdias55 <gd55@hw.ac.uk>
2020-11-26 12:01:03 +0400

    Login/register implemented

--- database.js
+++ database.js
@@ -1,20 +1,34 @@
 const express = require("express");
-//const expressSession = require("express-session");
-//const mysql = require("mysql");
-const bodyParser = require('body-parser'); 
-const readDataParser = bodyParser();
 const app = express();
+const session = require("express-session");
+const mysql = require("mysql");
+const SQL = require("sql-template-strings");
+const bodyParser = require("body-parser"); 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/login-page.html");
 });
 
+app.get("/play", (req, res) => {
+    res.sendFile(__dirname + "/leaderboard.html");
+})
+
+app.use(session({
+    secret: "secret", 
+    resave: false, 
+    saveUninitialized: false
+}));
+
 app.listen(3000);
 
 
 //Creating a database connection
-var mysql = require('mysql');
-var con = mysql.createConnection ({
+const con = mysql.createConnection ({
     host : 'sql12.freemysqlhosting.net',
     user : 'sql12378281',
     password : '7nS8iX9Bav',
@@ -23,86 +37,77 @@ var con = mysql.createConnection ({
 
 con.connect(function (err) {
     if (err) throw err;
-    console.log("Connected");
+    console.log("Database connected");
 });
 
-//Form authorization and taking user details from the client HTML (login-page.html)
-app.post('/login', readDataParser, function(req, res){
- 
-    username = req.body.username;
-    password = req.body.password;
 
-    //Username sanitization
-    username = username.replace(";","");
-    username = username.replace("!","");
-    username = username.replace("","");
-    username = username.replace("#","");
-    username = username.replace("$","");
-    username = username.replace("%","");
-    username = username.replace("^","");
-    username = username.replace("&","");
-    username = username.replace("*","");
-    username = username.replace("(","");
-    username = username.replace(")","");
-    username = username.replace("@","");
-    username = username.replace("=","");
-    username = username.replace("{","");
-    username = username.replace("}","");
-    username = username.replace(">","");
-    username = username.replace("<","");
-    username = username.replace(":","");
+// Login/Register
+// References: 
+// [1] https://codeshack.io/basic-login-system-nodejs-express-mysql/
+// [2] https://medium.com/technoetics/handling-user-login-and-registration-using-nodejs-and-mysql-81b146e37419
+app.post('/login', function(req, res){
+    let username = req.body.username;
+    const password = req.body.password;
 
-    //To encrypt password
-    //var key = crypto.createCipher('aes-128-cbc', 'password');
-    //encryptedPassword = key.update(password, 'utf8', 'hex')
-    //encryptedPassword += key.final('hex');
+    // Remove special characters from username
+    invalidChars = ";!#$%^&*()@={}<>:";
+    for (let i = 0; i < invalidChars.length; i++) {
+        username = username.replace(invalidChars[i] + "", "");
+    }
 
-    //Checking if the username exists to prevent multiple users with same username
-    var usernameCheck = "SELECT * from login WHERE Username = '" + username + "';";
-    con.query(usernameCheck, function(err, result){
+    // Check if account exists
+    const usernameExists = (SQL `SELECT username from accounts WHERE username=${username};`);
+    con.query(usernameExists, async function(err, result){
         if (err) throw err;
 
-        //If the user does exist
-        if(result.length){
-            //Checking if the username and password match
-            var passCheck = "SELECT * from login WHERE Password = '" + password + "';";
+        // If the account already exists, check password and login
+        if (result.length) {
+            // Get the stored password for the account
+            const getPassword = (SQL `SELECT * from accounts WHERE username=${username};`);
             
-            con.query(passCheck, function(err, result){
+            con.query(getPassword, async function(err, result){
                 if (err) throw err;
-                console.log(result + " password");
-                //If the password matches
-                if (result.length){
-                    console.log("one exists");
-                    //The game starts
-                    res.sendFile(__dirname + '/src/game.html');
-                }
+                 
+                // Compare password hashes
+                const comparePasswords = await bcrypt.compare(password, result[0].password);
 
-                //If passwords dont match, then error is shown to the user and login page restarts
-                else{
-                    console.log("Wrong password entered");
-                    res.sendFile(__dirname + '/client/index.html')
+                // If password was correct
+                if (comparePasswords) {
+                    console.log("Logged in");
+                    req.session.loggedIn = true;
+                    req.session.username = username;
+                    res.redirect("/play");
+                }
+                // Reload the page if the password was incorrect
+                else {
+                    console.log("Wrong password");
+                    res.redirect("/");
                 }
             });
         }
 
-        //If the user does not exist
-        else{
-            console.log("none exists");
+        // If the account does not exist, create a new account
+        else {
+            // Hash password
+            const encryptedPassword = await bcrypt.hash(password, saltRounds);
 
             //A new user is added to the table
-            con.query("SELECT * FROM login;", function (err, result) {
+            const newUser = (SQL `INSERT INTO accounts VALUES(${username}, ${encryptedPassword});`);
+            con.query(newUser, function (err, result) {
                 if (err) throw err;
-                //res.send(result);
-                console.log(result);
-            })
 
-            var sql = "INSERT INTO login (Username, Password) VALUES ('"+ username +"','"+password+"');";
-            con.query(sql, function (err, result) {
-                if (err) throw err;
-                console.log("1 record inserted");
+                // Insert initial score in leaderboard table
+                const setInitialScore = (SQL `INSERT INTO leaderboard VALUES(${username}, 0);`);
+                con.query(setInitialScore, function(err, result) {
+                    if (err) throw err;
+                });
+                
+                console.log("New accound created");
+                // Redirect to game page
+                req.session.loggedIn = true;
+                req.session.username = username;
+                res.redirect("/play");
             });
-            //We jump to the game HTML
-            res.sendFile(__dirname + '/client/gamebegin.html');
         }
     });
 });
